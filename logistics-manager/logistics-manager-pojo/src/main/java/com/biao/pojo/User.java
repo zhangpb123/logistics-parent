d73ed9edb8f349e5b557b9e78b441bc931b9b664
biao <zhangpb138@163.com>
2021-05-12 20:49:29 +0800

    v1.1完成逆向工程

---
+++ logistics-manager/logistics-manager-pojo/src/main/java/com/biao/pojo/User.java
@@ -0,0 +1,53 @@
+package com.biao.pojo;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String address;
+
+    private String gender;
+
+    private String hobby;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender == null ? null : gender.trim();
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby == null ? null : hobby.trim();
+    }
+}
